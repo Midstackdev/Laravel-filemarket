7b6f6e5ca0faa69a077436bdb5625d68ef4bf082
Midstackdev <midstackdev@gmail.com>
2019-11-01 12:28:16 +0000

    Paid files with stripe

--- app/Http/Controllers/Account/MarketplaceConnectController.php
+++ app/Http/Controllers/Account/MarketplaceConnectController.php
@@ -3,12 +3,49 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client as Guzzle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class MarketplaceConnectController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware(['auth', 'has.marketplace']);
+	}
+
     public function index()
     {
+    	session(['stripe_token' => Str::random(60)]);
+
     	return view('account.marketplace.index');
+    }
+
+    public function store(Request $request, Guzzle $guzzle)
+    {
+    	if (!$request->code) {
+    		return redirect()->route('account.connect');
+    	}
+
+    	if ($request->state !== session('stripe_token')) {
+    		return redirect()->route('account.connect');
+    	}
+
+    	$stripeRequest = $guzzle->request('POST', 'https://connect.stripe.com/oauth/token', [
+    		'form_params' => [
+    			'client_secret' => config('services.stripe.secret'),
+    			'code' => $request->code,
+    			'grant_type' => 'authorization_code'
+    		]
+    	]);
+
+    	$stripeResponse = json_decode($stripeRequest->getBody());
+
+    	$request->user()->update([
+    		'stripe_id' => $stripeResponse->stripe_user_id,
+    		'stripe_key' => $stripeResponse->stripe_publishable_key,
+    	]);
+
+    	return redirect()->route('account')->withSuccess('You have connected your stripe account');
     }
 }
---
+++ resources/views/files/partials/_checkout_form.blade.php
@@ -0,0 +1,18 @@
+<form action="{{ route('checkout.free', $file) }}" method="post">
+	@csrf
+	$ {{ $file->price }}
+
+	<script
+		src="https://checkout.stripe.com/checkout.js"
+		class="stripe-button"
+		data-key="{{ $file->user->stripe_key }}"
+		data-amount="{{ $file->price * 100 }}"
+		data-name="{{ $file->title }}"
+		data-description="{{ $file->overview_short }}"
+		data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+		data-locale="auto"
+		data-currency="usd"
+	>
+		
+	</script>
+</form>
